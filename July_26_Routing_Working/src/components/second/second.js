17f114c5f5ef1485ae6353811941e8241706b6d9
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-07-26 09:32:47 +0530

    code

---
+++ July_26_Routing_Working/src/components/second/second.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menu from '../menu/menu';
+
+const Second = () => {
+  return(
+    <div>
+      <Menu />
+      <p>This is My Second Component...</p>
+    </div>
+  )
+}
+export default Second;
